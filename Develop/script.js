7d61d3d4972d297402fb26330347522ab44a3a5c
cell gonzales <aragonzales.k@gmail.com>
2022-02-20 13:40:01 -0800

    added prompts for user

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,30 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  window.alert("Please answer the following questions for preferred password criteria.");
+//use variables to leverage user input
+ var length= window.prompt("How many characters would you like? Choose between 8 and 126.")
+ //this statement makes sure the input is within min and max parameters
+ if (length >=8 && length <=128){
+ }
+ else{
+   (window.prompt("Choose between 8 and 126!"))
+ };
+
+  var upperCase = window.confirm("Do you want uppercase letters?")
+  
+
+  var lowerCase = window.confirm("How about lowercase letters?")
+ 
+
+  var numbers = window.confirm("Do you want numbers?")
+ 
+
+  var specialCharacters = window.confirm("What about special characters?")
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
